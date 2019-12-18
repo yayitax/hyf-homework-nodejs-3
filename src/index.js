bdfaf58959b2ca5993ba622c0cd0b51c3db24dc7
Ximena <yayitax_cadena95@hotmail.com>
2019-12-18 15:47:51 -0700

    Added /users GET request

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
-    console.log("server is running")
-})
-
 app.get('/', function(req, res) {
     res.send('Hello World!')
+})
+
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: []
+    })
+})
+
+
+app.listen(3000, function() {
+    console.log("server is running")
 })
